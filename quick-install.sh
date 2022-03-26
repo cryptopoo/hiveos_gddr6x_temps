edbc9926412bc62295f5eb0eda281c439192c9df
cryptopoo <102094145+cryptopoo@users.noreply.github.com>
2022-03-26 10:36:14 +0100

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -2,7 +2,7 @@
 
 cd /tmp && \
 rm -rf hiveos_gddr6x_temps-main && \
-wget https://github.com/The-Sickness/hiveos_gddr6x_temps/archive/refs/heads/main.zip -O main.zip && \
+wget https://github.com/cryptopoo/hiveos_gddr6x_temps/archive/refs/heads/main.zip -O main.zip && \
 unzip main.zip && \
 cd hiveos_gddr6x_temps-main && \
 bash install.sh

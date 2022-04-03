f78d5855b72a589f1538b90368de27740cf994b0
cryptopoo <102094145+cryptopoo@users.noreply.github.com>
2022-04-03 16:52:09 +0200

    Added version check to prevent installation on HiveOS version >= 214

--- patch-autofan.sh
+++ patch-autofan.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-if [ ! -f /hive/sbin/autofan.orig ] || [ "$(cat /hive/sbin/autofan /hive/sbin/autofan.orig | grep AUTOFAN_VERSION= | uniq | wc -l)" != "1" ]; then
-    cp /hive/sbin/autofan /hive/sbin/autofan.orig
-fi
 
-patch -d/ -p0 -s -N -r- < /opt/gddr6x_temps/autofan.patch && echo "Autofan script patched succesfully!"
+HIVEOS_VERSION=$(apt show hive 2>1 | grep Version | cut -d '-' -f2)
+
+if [ "${HIVEOS_VERSION}" -lt "214" ]; then
+    if [ ! -f /hive/sbin/autofan.orig ] || [ "$(cat /hive/sbin/autofan /hive/sbin/autofan.orig | grep AUTOFAN_VERSION= | uniq | wc -l)" != "1" ]; then
+        cp /hive/sbin/autofan /hive/sbin/autofan.orig
+    fi
+
+    patch -d/ -p0 -s -N -r- < /opt/gddr6x_temps/autofan.patch && echo "Autofan script patched succesfully!"
+else
+    echo "Your current version of HiveOS '${HIVEOS_VERSION}' doesn't require this patch."
+fi
--- quick-install.sh
+++ quick-install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-wget -O - "https://github.com/cryptopoo/hiveos_gddr6x_temps/archive/refs/tags/v1.2.0.tar.gz" | tar -C /tmp -xzf - && \
-cd /tmp/hiveos_gddr6x_temps-1.2.0 && \
-./install.sh
+HIVEOS_VERSION=$(apt show hive 2>1 | grep Version | cut -d '-' -f2)
+
+if [ "${HIVEOS_VERSION}" -lt "214" ]; then
+    wget -O - "https://github.com/cryptopoo/hiveos_gddr6x_temps/archive/refs/tags/v1.2.0.tar.gz" | tar -C /tmp -xzf - && \
+    cd /tmp/hiveos_gddr6x_temps-1.2.0 && \
+    ./install.sh
+else
+    echo "Your current version of HiveOS '${HIVEOS_VERSION}' doesn't require this patch."
+fi
